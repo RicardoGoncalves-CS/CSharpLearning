83ed8be6feda4fe754bcc11a71f55fc22a15c14d
Ricardo Goncalves <ricardogoncalves.cds@outlook.com>
2023-02-22 23:04:27 +0000

    First commit of Week 1 Business Week

---
+++ Week 1 Business Week/Shell/MyShellScript.ps1
@@ -0,0 +1,19 @@
+﻿$folderPath = "~\Documents\Organize"
+
+if (!(Test-Path $folderPath))
+{
+md $folderPath
+}
+
+$date = Get-Date -Format "dd-MM-yyyy"
+
+$fullPath = $folderPath + "\" + $date
+
+if (!(Test-Path $fullPath))
+{
+md $fullPath
+echo "Folder $fullPath was successfully created."
+}
+
+mv ~\Desktop\* $fullPath
+echo "All files and folders on Desktop have been moved to $fullPath."
