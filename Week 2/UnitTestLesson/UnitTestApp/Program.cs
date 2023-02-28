931af772a0b3e992b4aa1e33a1f98c5511142ba6
Ricardo Goncalves <ricardogoncalves.cds@outlook.com>
2023-02-28 10:21:14 +0000

    Created UnitTestLesson App and Project

---
+++ Week 2/UnitTestLesson/UnitTestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UnitTestApp;
+
+public class Program
+{
+    static void Main()
+    {
+        int timeOfDay = 21;
+        if (timeOfDay >= 5 && timeOfDay <= 12)
+        {
+            Console.WriteLine("Good morning!");
+        }
+        else if (timeOfDay >= 12 && timeOfDay <= 18)
+        {
+            Console.WriteLine("Good afternoon!");
+        }
+        else
+        {
+            Console.WriteLine("Good evening!");
+        }
+    }
+}
