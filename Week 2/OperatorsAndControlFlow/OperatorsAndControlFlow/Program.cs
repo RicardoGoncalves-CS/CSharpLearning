6ea54611337ae57f469fc27f91eb690cbb210271
Ricardo Goncalves <ricardogoncalves.cds@outlook.com>
2023-02-28 15:41:12 +0000

    Created GetStones and GetStonesLeft methods

--- Week 2/OperatorsAndControlFlow/OperatorsAndControlFlow/Program.cs
+++ Week 2/OperatorsAndControlFlow/OperatorsAndControlFlow/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace OperatorsAndControlFlow
+﻿namespace OperatorsAndControlFlow;
+
+public class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        int x = 5;
+        int y = 5;
+
+        int a = x++; // post increment operator -> x is assigned before increment
+        int b = ++y; // pre increment operator -> y is incremented befor assignment
+
+        // Demo
+        Console.WriteLine("x = " + x + ", y = " + y);
+        Console.WriteLine("a = " + a + ", b = " + b);
+
+        // var is a placeholder for the data type. The compiler evaluates the data types in the operation to infer the var type.
+        var c = 10 / 3;
+        var d = 9.5 / 4;
+
+        Console.WriteLine("c is of type " + c.GetType());
+        Console.WriteLine("d is of type " + d.GetType());
+
+        // Exercise using GetStones() and GetStonesLeft()
+        int stones = GetStones(156); // expect stones to be 11
+        int poundsLeft = GetPoundsLeft(156); // expect stones to be 2
+
+        Console.WriteLine(stones);
+        Console.WriteLine(poundsLeft);
+    }
+
+    public static int GetStones(int totalPounds)
+    {
+        return totalPounds / 14;
+    }
+
+    public static int GetPoundsLeft(int totalPounds)
+    {
+        return totalPounds % 14;
     }
 }
