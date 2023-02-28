6ea64fb575e8401c93905ed9df81062b09c573e5
Ricardo Goncalves <ricardogoncalves.cds@outlook.com>
2023-02-28 13:42:55 +0000

    commiting the original TestFirstExercise

---
+++ Week 2/UnitTestLesson/UnitTestLab/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace UnitTestLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static string AvailableClassifications(int ageOfViewer)
+        {
+            string result;
+            if (ageOfViewer < 5)
+            {
+                result = "U films are available.";
+            }
+            else if (ageOfViewer < 9)
+            {
+                result = "U and PG films are available.";
+            }
+            else if (ageOfViewer < 13)
+            {
+                result = "U, PG, 12 & 15 films are available.";
+            }
+            else if (ageOfViewer < 16)
+            {
+                result = "U, PG, 12 & 15 films are available.";
+            }
+            else
+            {
+                result = "All films are available.";
+            }
+
+            return result;
+        }
+    }
+}
