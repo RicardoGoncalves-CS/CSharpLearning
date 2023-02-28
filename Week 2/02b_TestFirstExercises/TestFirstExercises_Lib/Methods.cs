7e75e7f117db0ce6b1f03960cdaa3c0643ed334b
Ricardo Goncalves <ricardogoncalves.cds@outlook.com>
2023-02-28 14:58:32 +0000

    TestFirstExercises solved

--- Week 2/02b_TestFirstExercises/TestFirstExercises_Lib/Methods.cs
+++ Week 2/02b_TestFirstExercises/TestFirstExercises_Lib/Methods.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TestFirstExercises
 {
+
     public class Methods
     {
         // implement this method so that it returns true if x is greater than or equal to y
         public static bool GreaterEqual(int x, int y)
         {
-            return false;
+            return x >= y;
         }
 
         // Implement this method so that it take an int as an input,
@@ -16,13 +18,18 @@ namespace TestFirstExercises
         // Return a double rounded to 3 decimal places.
         public static double BODMAS(int inputNumber)
         {
-            return 0;
+            double value = Convert.ToDouble(inputNumber);
+            double result = (Math.Pow(value, 2) + 101) / 7 - 4;
+            result = Math.Round(result, 3);
+            
+            return result;
         }
 
         // implement this method so that it returns true if num is even, otherwise false
         public static bool EvenOdd(int num)
         {
-            return false;
+            if (num % 2 == 0) return true;
+            else return false;
         }
 
         // implement this method so that it returns 
@@ -30,21 +37,42 @@ namespace TestFirstExercises
         // that are divisible by either 2 or 5
         public static int SumEvenFive(int max)
         {
-            return -1;
+            int current = 1;
+            int sum = 0;
+            
+            while(current <= max)
+            {
+                if(current % 2 == 0 || current % 5 == 0)
+                {
+                    sum += current;
+                }
+                current++;
+            }
+            return sum;
         }
 
         // implement this method so it returns true if input is "password"
         // regardless of case
         public static bool CheckInput(string input)
         {
-            return false;
+            input = input.ToLower();
+            return input == "password";
         }
 
         // implement this method so it returns the sum
         // of all the numbers in the list
         public static int SumList(List<int> list)
         {
-            return -1;
+            /*
+            int sum = 0;
+            foreach(int num in list)
+            {
+                sum += num;
+            }
+            return sum;
+            */
+
+            return list.Sum();
         }
     }
 }
--- Week 2/UnitTestLesson/UnitTestApp/Program.cs
+++ Week 2/UnitTestLesson/UnitTestApp/Program.cs
@@ -4,40 +4,31 @@ public class Program
 {
     static void Main()
     {
+        /*
         int inputValue = Int32.Parse(Console.ReadLine());
         string output = MyMethod(inputValue);
         Console.WriteLine(output);
-    }
-
-    public static string MyMethod(int timeOfDay)
-    {
-        /*
-        Assuming timeOfDay is valid and 0-23 we should create 3 tests for each band
-        Band1 = 0-5
-        Band2 = 5-12
-        Band3 = 12-18
-        Band4 = 18-23
         */
 
-        string greeting;
-
-        if (timeOfDay < 5)
-        {
-            greeting = "Good morning!";
-        }
-        else if (timeOfDay < 12)
-        {
-            greeting = "Good morning!";
-        }
-        else if (timeOfDay <= 18)
-        {
-            greeting = "Good afternoon!";
-        }
-        else
-        {
-            greeting = "Good evening!";
-        }
-
-        return greeting;
+        int result = SumEvenFive(9);
+        Console.WriteLine(result);
     }
+
+    public static int SumEvenFive(int max)
+    {
+        int current = 1;
+        int sum = 0;
+
+        while (current < max)
+        {
+            if ((current % 2 == 0) || (current % 5 == 0))
+            {
+                sum += current;
+            }
+            current++;
+        }
+        return sum;
+    }
+
+    
 }
