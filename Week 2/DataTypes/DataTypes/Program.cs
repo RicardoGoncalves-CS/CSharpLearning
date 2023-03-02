f03a4d574f93e9e1bfaa1f1c136fe20f5177df4a
Ricardo Goncalves <ricardogoncalves.cds@outlook.com>
2023-03-02 13:45:31 +0000

    Added DataType Lab Starter

--- Week 2/DataTypes/DataTypes/Program.cs
+++ Week 2/DataTypes/DataTypes/Program.cs
@@ -9,6 +9,22 @@ public class Program
         // Type safe - We cannot assign different typed data to a variable;
         // Memory safe - Memory size of variables are known;
 
+        // Example of underflow
+        byte aggro = 0;
+        Console.WriteLine("Ghandi's aggro is " + aggro);
+        aggro -= 1;
+        Console.WriteLine("Ghandi's aggro is " + aggro);
+
+        checked    // Checks a block for Overflow Exceptions
+        {
+            byte aggro2 = 0;
+            Console.WriteLine("Ghandi's aggro is " + aggro2);
+            aggro2 -= 1;
+            Console.WriteLine("Ghandi's aggro is " + aggro2);
+        }
+
+        #region numeric data types
+
         int a = 25;
         uint b = 25u;
         double c = 25.5d;
@@ -27,7 +43,9 @@ public class Program
         if (sum == 400_000) Console.WriteLine("Calculation success!");
         else Console.WriteLine("Calculation failure!");
 
-        #region Conversion
+        #endregion
+
+        #region Type Conversion
 
         int kangaroosInSydney = 500;
         long kangaroosInAustralia;
@@ -47,6 +65,31 @@ public class Program
 
         cake = kangaroosInAustralia;                    // safe conversion
         // kangaroosInAustralia = cake;                 // unsafe conversion
+
+        double x = 3.14159265359;
+        // unsafe type conversion using the casting operator()
+        float y = (float)x;
+
+        Console.WriteLine("X = " + x);
+        Console.WriteLine("Y = " + y);
+
+        int numCows = 300;
+        uint numCowsU = (uint)numCows;
+        byte numCowsB = (byte)numCows;
+
+        Console.WriteLine("numCows = " + numCows);
+        Console.WriteLine("numCowsU = " + numCowsU);
+        Console.WriteLine("numCowsB = " + numCowsB);
+
+        int studentLoan = -10000;
+        uint studentLoanU = (uint)studentLoan;
+
+        Console.WriteLine("studentLoan = " + studentLoan);
+        Console.WriteLine("studentLoanU = " + studentLoanU);
+
+        // Using the Convert class
+        numCowsB = Convert.ToByte(numCows);     // The Convert class do back-ground checks and will throw an Exception
+        int myFive = Convert.ToInt32("Five");
 
         #endregion
     }
---
+++ Week 2/DataTypes/StringsAndArrays/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace StringsAndArrays;
+
+public class Program
+{
+    static void Main()
+    {
+        string mySuperSecretPassword = "SuperSecretPassword!".ToLower();
+        char grade = 'A';
+
+        char firstLetter = mySuperSecretPassword[0];
+
+        foreach (char c in mySuperSecretPassword) Console.WriteLine(c);
+
+        mySuperSecretPassword = mySuperSecretPassword.ToUpper();
+        Console.WriteLine(mySuperSecretPassword);
+
+        // Exercise
+        // Trim off any leading or trailing spaces from myString
+        // Turn all the characters to Upper Case
+        // Replace all occurances of the letters 'L' and 'T' with '*'
+        // Find the index of the letter 'N', and delete all the characters after it
+        // Return the result
+        var myString = "          C# list fundamentals ";
+        Console.WriteLine(StringExercise(myString));
+
+        // String Builder
+        Console.WriteLine(StringBuilderExercise(myString));
+
+        // String Interpolation
+        string firstName = "James";
+        string lastName = "Bond";
+
+        Console.WriteLine($"My name is {lastName},\n{firstName} {lastName}!");  // \n : new line
+
+        // We can use @ instead of $ if we want to print every character literally including special characters
+        Console.WriteLine(@"My name is {lastName},\n{firstName} {lastName}!");
+
+        Console.WriteLine($"That will be {5M / 3:C} please");   // :C format as currency
+
+        // Parsing Strings
+        string input = Console.ReadLine();
+
+        // int numApples = Int32.Parse(input);
+        var tryParseOutput = Int32.TryParse(input, out int numApples);
+
+        if (tryParseOutput) Console.WriteLine(numApples);
+        else Console.WriteLine("Not an Integer!");
+    }
+
+    public static string StringExercise(string s)
+    {
+        string myString = s.Substring(0, s.IndexOf('n') + 1).Trim().ToUpper().Replace('L', '*').Replace('T', '*');
+        return myString;
+    }
+
+    public static string StringBuilderExercise(string s)
+    {
+        string trimmed = s.Trim();
+        string upper = trimmed.ToUpper();
+        int indexOfN = upper.IndexOf('N');
+
+        StringBuilder strBuilder = new StringBuilder(upper);
+
+        strBuilder.Replace('L', '*').Replace('T', '*').Remove(indexOfN + 1, strBuilder.Length - indexOfN - 1);
+
+        return strBuilder.ToString();
+    }
+}
