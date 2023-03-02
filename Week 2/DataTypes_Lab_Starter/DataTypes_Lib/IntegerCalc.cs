736b688270f87b97ccf1cc407bbac48d92dbb280
Ricardo Goncalves <ricardogoncalves.cds@outlook.com>
2023-03-02 20:05:55 +0000

    DataTypes_Lab solved

--- Week 2/DataTypes_Lab_Starter/DataTypes_Lib/IntegerCalc.cs
+++ Week 2/DataTypes_Lab_Starter/DataTypes_Lib/IntegerCalc.cs
@@ -6,33 +6,41 @@ namespace DataTypes_Lib
     {
         public static int Add(int num1, int num2)
         {
-            
-            return num1 + num2;
-            //throw new NotImplementedException();
+            return checked ( num1 + num2 );
         }
 
         public static int Subtract(int num1, int num2)
         {
-            return num1 - num2;
-            //throw new NotImplementedException();
+            return checked ( num1 - num2 );
         }
 
         public static int Multiply(int num1, int num2)
         {
-            return num1 * num2;
-            //throw new NotImplementedException();
+            return checked ( num1 * num2 );
         }
 
         public static int Divide(int num1, int num2)
         {
-            return num1 / num2;
-            //throw new NotImplementedException();
+            try
+            {
+                return checked ( num1 / num2 );
+            }
+            catch
+            {
+                throw new ArgumentException("Can't divide by zero");
+            }
         }
 
         public static int Modulus(int num1, int num2)
         {
-            return num1 % num2;
-            //throw new NotImplementedException();
+            try
+            {
+                return checked ( num1 % num2 );
+            }
+            catch
+            {
+                throw new ArgumentException("Can't modulo by zero");
+            }
         }
     }
 }
