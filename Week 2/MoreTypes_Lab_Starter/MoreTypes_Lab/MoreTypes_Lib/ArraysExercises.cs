4fa50a722ed6e498feec82db0b493a7c81983ffc
Ricardo Goncalves <ricardogoncalves.cds@outlook.com>
2023-03-03 00:24:10 +0000

    MoreTypes_Lab updates - Arrays and String Exercises complete

--- Week 2/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/ArraysExercises.cs
+++ Week 2/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/ArraysExercises.cs
@@ -8,19 +8,75 @@ namespace MoreTypes_Lib
         // returns a 1D array containing the contents of a given List
         public static string[] Make1DArray(List<string> contents)
         {
-            throw new NotImplementedException();
+            string[] strArr = new string[contents.Count];
+
+            for (int i = 0; i < contents.Count; i++)
+            {
+                strArr[i] = contents[i];
+            }
+
+            return strArr;
         }
 
         // returns a 3D array containing the contents of a given List
         public static string[,,] Make3DArray(int length1, int length2, int length3, List<string> contents)
         {
-            throw new NotImplementedException();
+            try
+            {
+                int counter = 0;
+
+                string[,,] str3D = new string[length1, length2, length3];
+
+                for (int i = 0; i < length1; i++)
+                {
+                    for (int j = 0; j < length2; j++)
+                    {
+                        for (int k = 0; k < length3; k++)
+                        {
+                            str3D[i, j, k] = contents[counter];
+                            counter++;
+                        }
+                    }
+                }
+
+                return str3D;
+            }
+            catch
+            {
+                throw new ArgumentException("Number of elements in list must match array size");
+            }
         }
 
         // returns a jagged array containing the contents of a given List
         public static string[][] MakeJagged2DArray(int countRow1, int countRow2, List<string> contents)
         {
-            throw new NotImplementedException();
+            try
+            {
+                int contentsCounter = 0;
+                int index = 0;
+                int rowCounter = countRow1;
+
+                string[][] jaggedArr = new string[2][];
+
+                jaggedArr[0] = new string[countRow1];
+                jaggedArr[1] = new string[countRow2];
+
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < rowCounter; j++)
+                    {
+                        jaggedArr[index][j] = contents[contentsCounter];
+                        contentsCounter++;
+                    }
+                    rowCounter = countRow2;
+                    index++;
+                }
+                return jaggedArr;
+            }
+            catch
+            {
+                throw new ArgumentException("Number of elements in list must match array size");
+            }
         }
     }
 }
--- Week 2/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/StringExercises.cs
+++ Week 2/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/StringExercises.cs
@@ -7,31 +7,55 @@ namespace MoreTypes_Lib
         // manipulates and returns a string - see the unit test for requirements
         public static string ManipulateString(string input, int num)
         {
-            throw new NotImplementedException();
+            string numericString = "";
+
+            for (int i = 0; i < num; i++)
+            {
+                numericString += i;
+            }
+
+            return input.Trim().ToUpper() + numericString;
         }
 
         // returns a formatted address string given its components
         public static string Address(int number, string street, string city, string postcode)
         {
-            throw new NotImplementedException();
+            return $"{number} {street}, {city} {postcode}.";
         }
+
         // returns a string representing a test score, written as percentage to 1 decimal place
         public static string Scorer(int score, int outOf)
         {
-            throw new NotImplementedException();
+            return $"You got {score} out of {outOf}: {Math.Round((float)score*100/outOf, 1)}%";
         }
 
         // returns the double represented by the string, or -999 if conversion is not possible
         public static double ParseNum(string numString)
         {
-            throw new NotImplementedException();
+            if (Double.TryParse(numString, out double num)) return num;
+            else return -999;
         }
 
         // Returns the a string containing the count of As, Bs, Cs and Ds in the parameter string
         // all other letters are ignored
         public static string CountLetters(string input)
         {
-            throw new NotImplementedException();
+            int countA = 0;
+            int countB = 0;
+            int countC = 0;
+            int countD = 0;
+
+            input = input.ToUpper();
+
+            foreach(char letter in input)
+            {
+                if (letter == 'A') countA += 1;
+                if (letter == 'B') countB += 1;
+                if (letter == 'C') countC += 1;
+                if (letter == 'D') countD += 1;
+            }
+
+            return $"A:{countA} B:{countB} C:{countC} D:{countD}";
         }
     }
 }
