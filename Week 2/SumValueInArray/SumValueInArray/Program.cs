1b9641fe97807ba7afab7bd7c6ebc59dbe2dc9fa
Ricardo Goncalves <ricardogoncalves.cds@outlook.com>
2023-03-06 12:13:02 +0000

    Created SumValuesInArray solution

---
+++ Week 2/SumValueInArray/SumValueInArray/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SumValueInArray;
+
+public class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5, 3 };
+        Console.WriteLine(SumValuesInArray(array, 0, 6));
+    }
+
+    public static int SumValuesInArray(int[] arr, int n1, int n2)
+    {
+        int total = 0;
+        
+        for (int i = n1; i < n2; i++)
+        {
+            total += arr[i];
+        }
+
+        return total;
+    }
+}
