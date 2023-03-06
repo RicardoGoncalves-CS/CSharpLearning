925e1c5fc2cf51538fb29f72c1a326dc4b0a1534
Ricardo Goncalves <ricardogoncalves.cds@outlook.com>
2023-03-06 13:30:26 +0000

    Created a program to receive numeric input, store into an array and print the result in the console

---
+++ More Exercises/Arrays/StoreToArrayAndPrint/StoreToArrayAndPrint/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace StoreToArrayAndPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myValues = ReadValues();
+            PrintArray(myValues);
+        }
+
+        public static int[] ReadValues()
+        {
+            List<int> inputList = new List<int>();
+            string input = "";
+            int counter = 1;
+
+            Console.WriteLine("Enter SAVE to store values and exit");
+
+            while (input != "SAVE")
+            {
+                Console.WriteLine("Enter an integer to store: ");
+                input = Console.ReadLine().ToUpper();
+
+                if(input != "SAVE")
+                {
+                    try
+                    {
+                        int number = Convert.ToInt32(input);
+                        inputList.Add(number);
+                        Console.WriteLine($"Number {counter} successfully added!");
+                        counter++;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Input must be of Integer type!");
+                    }
+                }
+            }
+
+            int[] inputArr = new int[inputList.Count];
+
+            for(int i = 0; i < inputArr.Length; i++)
+            {
+                inputArr[i] = inputList[i];
+            }
+
+            return inputArr;
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            foreach(int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
