56dc7c02648ccdf356f304cce8d207c1075898d3
RicardoGoncalves-CS <RicGoncalves@proton.me>
2023-03-15 18:43:08 +0000

    Comitting week4 mini project

---
+++ More Exercises/Model/Model/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace Model
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arr = new int[5000];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next();
+            }
+
+            BubbleSorter bSort = new BubbleSorter();
+            int[] sorted = bSort.Sort(arr);
+            long elapsedBubbleSorter = bSort.Timelapse;
+
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Elapsed time: {elapsedBubbleSorter} ms");
+            Console.WriteLine();
+
+            MergeSorter mSort = new MergeSorter();
+            int[] sorted2 = mSort.Sort(arr);
+            long elapsedMergeSorter = mSort.Timelapse;
+
+            foreach (var item in sorted2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Elapsed time: {elapsedMergeSorter} ms");
+            Console.WriteLine();
+
+            Sorter myMergeSorter = SorterFactory.CreateSorter("mergesort");
+            myMergeSorter = (MergeSorter)myMergeSorter;
+            int[] sorted3 = myMergeSorter.Sort(arr);
+            long elapsedFactorySorter = myMergeSorter.Timelapse;
+
+            foreach (var item in sorted3)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Elapsed time: {elapsedFactorySorter} ms");
+            Console.WriteLine();
+
+            Console.WriteLine($"{myMergeSorter}");
+        }
+    }
+}
---
+++ Week 4/DesignPatternExamples/FactoryMethodExample/Beverages/Americano.cs
@@ -0,0 +1,19 @@
+﻿using FactoryMethodExample.DrinkShop;
+
+namespace FactoryMethodExample.Beverages
+{
+    internal class Americano : Beverage
+    {
+        public override string Name => "Americano";
+
+        public override void Prepare()
+        {
+            Console.WriteLine("Press the Americano button.. schhhhhhhhh gurgle.");
+        }
+
+        public override void Serve()
+        {
+            Console.WriteLine("Pass the Americano to the customer.");
+        }
+    }
+}
