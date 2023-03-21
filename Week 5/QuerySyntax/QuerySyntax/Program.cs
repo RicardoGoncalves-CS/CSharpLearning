bcba0661be7710e6166cb206bc2e81bfedd27771
RicardoGoncalves-CS <RicGoncalves@proton.me>
2023-03-21 12:17:52 +0000

    LINQ Walkthrough

---
+++ Week 5/QuerySyntax/QuerySyntax/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace QuerySyntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+}
