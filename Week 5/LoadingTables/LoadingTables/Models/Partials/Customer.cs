43732a221be83f06ae1746cf345aeb3e60ae04de
RicardoGoncalves-CS <RicGoncalves@proton.me>
2023-03-22 10:04:50 +0000

    Comit LoadingTables starter

---
+++ Week 5/LoadingTables/LoadingTables/Models/Partials/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadingTables.Models
+{
+    public partial class Customer
+    {
+        public override string ToString()
+        {
+            return $"{CustomerId} - {ContactName} - {CompanyName} - {City}";
+        }
+    }
+}
