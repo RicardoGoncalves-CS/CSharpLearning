43359603bf603f39b9b8c9b31d4689f95341f33c
RicardoGoncalves-CS <RicGoncalves@proton.me>
2023-03-22 14:56:33 +0000

    Southwind database created

---
+++ Week 5/EF_ModelFirst_Starter/EF_ModelFirst/Migrations/20230322141609_Added country property to customers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_ModelFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedcountrypropertytocustomers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Customers");
+        }
+    }
+}
---
+++ Week 5/EF_ModelFirst_Starter/EF_ModelFirst/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EF_ModelFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SouthwindContext())
+            {
+                List<Customer> customersList = new List<Customer>()
+                {
+                    new Customer
+                    {
+                        CustomerId = "C1",
+                        ContactName = "Paul Smith",
+                        City = "London",
+                        PostalCode = "1234"
+                    },
+                    new Customer
+                    {
+                        CustomerId = "C2",
+                        ContactName = "Alan Grant",
+                        City = "Manchester",
+                        PostalCode = "4545"
+                    },
+                    new Customer
+                    {
+                        CustomerId = "C3",
+                        ContactName = "William Wallace ",
+                        City = "Edinbrough",
+                        PostalCode = "9876"
+                    },
+                    new Customer
+                    {
+                        CustomerId = "C4",
+                        ContactName = "Brandon Johnson",
+                        City = "Leeds",
+                        PostalCode = "5050"
+                    },
+                    new Customer
+                    {
+                        CustomerId = "C5",
+                        ContactName = "Clarice Munchies",
+                        City = "Birmingham",
+                        PostalCode = "7841"
+                    }
+
+                };
+
+                db.AddRange(customersList);
+                db.SaveChanges();
+            };
+        }
+    }
+}
+
