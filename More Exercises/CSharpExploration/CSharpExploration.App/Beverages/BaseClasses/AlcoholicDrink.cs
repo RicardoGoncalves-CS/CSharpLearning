19dd640cb536874478c77bf14e80a040bfce9eb3
RicardoGoncalves-CS <RicGoncalves@proton.me>
2023-03-24 18:44:40 +0000

    deleted folder

--- More Exercises/CSharpExploration/CSharpExploration.App/Beverages/BaseClasses/AlcoholicDrink.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CSharpExploration.App.Beverages.BaseClasses;
-
-internal abstract class AlcoholicDrink : Beverage
-{
-    protected double _alcohol;
-    protected string _alcoholUnit = "% ABV";
-
-    public abstract double GetAlcohol();
-}
