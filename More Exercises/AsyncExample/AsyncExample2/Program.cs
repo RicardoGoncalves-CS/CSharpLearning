c5126be4d898a7562c2bab78d0829bef7bcc4a12
RicardoGoncalves-CS <RicGoncalves@proton.me>
2023-03-26 18:57:20 +0100

    Commit AsyncExample2 and 3

---
+++ More Exercises/AsyncExample/AsyncExample2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+
+namespace AsyncExample2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            await PrepareBreakfast();
+            Console.WriteLine("Breakfast is ready");
+            sw.Stop();
+            Console.WriteLine($"Breakfast took {sw.ElapsedMilliseconds} ms to prepare");
+        }
+
+        public static void PrepareBread()
+        {
+            Console.WriteLine("Cut the bread open");
+            Console.WriteLine("Spread butter on the bread");
+        }
+
+        public static void PrepareCup()
+        {
+            Console.WriteLine("The cup is ready for the tea");
+        }
+
+        /* We can observe that both cookBacon and brewTea tasks take wait 4 seconds
+         * to complete which they will process in parallel;
+         */
+        public static async Task PrepareBreakfast()
+        {
+            var cookBacon = CookBaconAsync();
+            PrepareCup();
+            var brewTea = TeaBrewAsync();
+            PrepareBread();
+            var bacon = await cookBacon;
+            Console.WriteLine(bacon.IsCooked());
+            var tea = await brewTea;
+            Console.WriteLine(tea.IsBrewed());
+        }
+
+        public static async Task<Bacon> CookBaconAsync()
+        {
+            Console.WriteLine("Bacon is in the oven");
+            await Task.Delay(TimeSpan.FromSeconds(4));
+            return new Bacon();
+        }
+
+        public static async Task<Tea> TeaBrewAsync()
+        {
+            Console.WriteLine("Tea is brewing");
+            await Task.Delay(TimeSpan.FromSeconds(4));
+            return new Tea();
+        }
+    }
+
+    public class Bacon
+    {
+        public string IsCooked()
+        {
+            return "Bacon is cooked";
+        }
+    }
+
+    public class Tea
+    {
+        public string IsBrewed()
+        {
+            return "Tea is brewed";
+        }
+    }
+}
---
+++ More Exercises/AsyncExample/AsyncExample3/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace AsyncExample3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = DownloadDataAsync("https://bbc.co.uk");
+            Console.WriteLine(data.Result.Substring(0, 48));
+
+            Console.WriteLine();
+
+            var factResult = ComputeFactorialAsync(10);
+            Console.WriteLine(factResult.Result);
+
+            async Task<string> DownloadDataAsync(string url)
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    return await response.Content.ReadAsStringAsync();
+                }
+            }
+
+
+            async Task<int> ComputeFactorialAsync(int n)
+            {
+                return await Task.Run(() =>
+                {
+                    int result = 1;
+
+                    for (int i = 1; i <= n; i++)
+                    {
+                        result *= i;
+                    }
+
+                    return result;
+                });
+            }
+        }
+    }
+}
