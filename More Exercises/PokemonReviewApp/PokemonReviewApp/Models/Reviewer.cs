15c40a8771ae48ccf7c6bc351ac1d938e1f5e486
RicardoGoncalves-CS <RicGoncalves@proton.me>
2023-03-29 18:19:21 +0100

    Initial commit of PokemonReviewApp

---
+++ More Exercises/PokemonReviewApp/PokemonReviewApp/Models/Reviewer.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace PokemonReviewApp.Models;
+
+public class Reviewer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+
+    public ICollection<Review> Reviews { get; set; }
+}
